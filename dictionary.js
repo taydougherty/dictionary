e7034976e426b1b6a8f2a1d972e4374dc28574cc
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-07-17 12:23:47 -0700

    updated to include streams to process larger data sets

--- dictionary.js
+++ dictionary.js
@@ -10,41 +10,95 @@
 // pattern - all words are capitals
 // figure out which data structure to use (objects vs arrays)
 
+// require necessary packages
 var fs = require("fs");
+var readline = require('linebyline');
+rl = readline('rawDictionaryData.txt');
 
-var text = fs.readFileSync("rawDictionaryData.txt", "utf-8");
-var word = text.match(/[A-Z]+\;?\'?S?\s?\-?[A-Z]+?\r/g);
-// console.log(word);
+// set variable for definition to append into
+var defn = "";
 
-for (var i = 0; i < word.length-1; i++) {
-  var first = text.indexOf(word[i]);
-  var second = text.indexOf(word[i+1]);
+// set variable for reading the lines in definition if multiple
+var inDefn = false;
 
-  var findDefn = text.slice(first, second).split("\n\r");
+// open the csv file used to append data
+fs.open('editedDictionaryData.csv', 'a', (err, fd) => {
+    if (err) throw err;
 
-  // console.log(word[i]);
+    // read through each line from the txt file
+    rl.on('line', function (line) {
 
-  fs.appendFile("editedDictionaryData.csv", word[i], function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    // } else {
-    //   console.log("added to the csv!")
-    // }
-  });
+        // if the line has all capital letters it is a word
+        var word = line.match(/[A-Z]+\;?\'?S?\s?\-?[A-Z]+?/g);
 
-  findDefn.forEach(element => {
-    if (element.slice(0,7) === "\nDefn: ") {
-      var result = element;
-      // console.log(element);
+        // if a word exists, return the array as a string
+        if (word !== null) {
 
-      fs.appendFile("editedDictionaryData.csv", result, function(err) {
-        if (err) {
-          return console.log(err);
-        } else {
-          console.log("added to the csv!")
+            word = word.join("");
+            // console.log(word);
+            
+            // apend the word to the file
+            fs.appendFile(fd, word + "\n", function (err) {
+                if (err) {
+                    return console.log(err);
+                } else {
+                  // console.log("added to the csv!")
+                }
+            });
+    
         }
-      });
-    }
-  });
-};
+
+        // if the lines are still in the definition, append the line to the definition string
+        if (inDefn) {
+            defn += line;
+        }
+        
+        // check if the line begin with "Defn:" for definition
+        if (line.slice(0,5) === "Defn:") {
+            // if true, append line to definition string
+            defn += line;
+            // set variable to true
+            inDefn = true;
+        }
+        
+        // if new line is not part of the definition, append the definition string to the file and set inDefinition to false
+        if (inDefn && (line === "")) {
+
+            inDefn = false;
+            fs.appendFile(fd, defn + "\n\n", function (err) {
+                if (err) {
+                    return console.log(err);
+                } else {
+                  // console.log("added to the csv!")
+                }
+            });
+
+            defn = "";
+        }
+    })
+    .on('error', function (err) {
+        console.log('Error while reading file.', err);
+    })
+    .on('close', function (line) {
+        // when finished reading the lines
+        inDefn = false;
+        // console.log(definition);
+        
+        // append the remaining definition string into the csv file
+        fs.appendFile(fd, defn, function (err) {
+            if (err) {
+                return console.log(err);
+            } else {
+              // console.log("added to the csv!")
+            }
+        });
+
+        // set the definition back to an empty string
+        defn = "";
+
+        // close the csv file
+        fs.close(fd, (err) => {
+            if (err) throw err;
+        });
+    });
+});
